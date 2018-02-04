b28d76a8683357752e7eac9a19fe0d152746c3a3
Juanmi Rodriguez Cerón <juanmi.rodriguez.ceron@gmail.com>
2018-02-04 23:25:49 +0100

    step2, before refactoring

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Developed in Javascript, following https://github.com/kanaka/mal
 Just for fun and learning, because I want to learn Lisp and this looks like a fun way to do it.
+
+Estoy pensando que podría crear un Lisp en español, por diversión y porque tal vez sería una buena forma de enseñar a niños hispanos monolingües. Tomar lecciones de The Little Schemer, de el libro de Logo o cosas así y hacer unas lecciones totalmente en español, con el lenguaje también en español, y Lisp... soñar es gratis :)
--- step2_eval.js
+++ step2_eval.js
@@ -19,23 +19,37 @@ rl.on('close', () => process.exit(0))
 
 var READ = line => reader.read_str(line)
 
-var EVAL = line => line
+var EVAL = (line, env) => {
+  if(line && line.length > 0) {
+    const evaled_list = eval_ast(line, env)
+    return evaled_list[0].apply(null, evaled_list.slice(1))
+  }
+  if(line && line.length === 0) {
+    return line
+  }
+  return eval_ast(line, env)
+}
 
 var PRINT = line => console.log(printer.print_str(line))
 
 const repl_env = {
-  '+': (a,b) => a + b,
-  '-': (a,b) => a - b,
-  '*': (a,b) => a * b,
-  '/': (a,b) => a / b,
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '*': (a, b) => a * b,
+  '/': (a, b) => a / b,
 }
 
-function eval_ast(ast) {
-  
+function eval_ast(ast, env) {
+  if (ast && ast.name) {
+    return env[ast.name]
+  } else if (Array.isArray(ast)) {
+    return ast.map(elem => EVAL(elem, env))
+  }
+  return ast
 }
 
 function rep(line) {
-  PRINT(EVAL(READ(line), rep_env))
+  PRINT(EVAL(READ(line), repl_env))
 }
 
 rl.prompt()
